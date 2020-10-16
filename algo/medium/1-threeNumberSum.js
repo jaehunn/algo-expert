7ffa6cf837d4549aaac0d837a1ce81b0ff8a92bc
bangjaehun <qkdwogns98@gmail.com>
2020-10-16 12:20:56 +0900

    ...

--- algo/medium/1-threeNumberSum.js
+++ algo/medium/1-threeNumberSum.js
@@ -24,17 +24,15 @@ function threeNumberSum(array, targetSum) {
 // O(n^2) / O(n)
 function _threeNumberSum(array, targetSum) {
   array.sort((a, b) => a - b);
-  let count = 0;
+
   const triplets = [];
   for (let i = 0; i < array.length - 2; i++) {
     let left = i + 1;
     let right = array.length - 1;
 
-    console.log(count);
     while (left < right) {
       const currentSum = array[i] + array[left] + array[right];
 
-      count += 1;
       if (currentSum === targetSum) {
         triplets.push([array[i], array[left], array[right]]);
         left++;
@@ -49,5 +47,3 @@ function _threeNumberSum(array, targetSum) {
 
   return triplets;
 }
-
-_threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0);
