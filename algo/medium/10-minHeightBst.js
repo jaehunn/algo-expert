23e047ffc07b5f700a11178373c013c3ec25c478
bangjaehun <qkdwogns98@gmail.com>
2020-10-28 12:10:10 +0900

    10 11

--- algo/medium/10-minHeightBst.js
+++ algo/medium/10-minHeightBst.js
@@ -14,7 +14,79 @@ function minHeightBst(array) {
   return bst;
 }
 
-// other WIP
+// O(n log n) / O(n)
+function _minHeightBst(array) {
+  return _constructMinHeightBst(array, null, 0, array.length - 1);
+}
+
+function _constructMinHeightBst(array, bst, startIdx, endIdx) {
+  if (endIdx < startIdx) return;
+
+  const midIdx = Math.floor((startIdx + endIdx) / 2);
+  const valueToAdd = array[minIdx];
+
+  if (bst === null) {
+    bst = new BST(valueToAdd);
+  } else {
+    bst.insert(valueToAdd);
+  }
+
+  _constructMinHeightBst(array, bst, startIdx, midIdx - 1);
+  _constructMinHeightBst(array, bst, midIdx + 1, endIdx);
+
+  return bst;
+}
+
+// 외부 함수로 값을 넘겨야한다.(최종 결과) return bst 로 통로만들기
+
+// O(n) / O(n)
+function __minHeightBst(array) {
+  return __constructMinHeightBst(array, null, 0, array.length - 1);
+}
+
+function __constructMinHeightBst(array, bst, startIdx, endIdx) {
+  if (endIdx > startIdx) return;
+
+  const midIdx = Math.floor((startIdx + endIdx) / 2);
+  const newBstNode = new BST(array[midIdx]);
+
+  if (bst === null) bst = newBSTNode;
+  else {
+    if (array[midIdx] < bst.value) {
+      bst.left = newBstNode;
+      bst = bst.left;
+    } else {
+      bst.right = newBstNode;
+      bst = bst.right;
+    }
+  }
+
+  __constructMinHeightBst(array, bst, startIdx, midIdx - 1);
+  __constructMinHeightBst(array, bst, midIdx + 1, endIdx);
+
+  return bst;
+}
+
+// 상황에 따라 한번의 연산이 동작한다면 상위 스코프에 공통연산으로 등록한다
+
+// O(n) / O(n)
+function ___minHeightBst(array) {
+  return ___constructMinHeightBst(array, 0, array.length - 1);
+}
+
+function ___constructMinHeightBst(array, startIdx, endIdx) {
+  if (endIdx < startIdx) return null;
+
+  const midIdx = Math.floor((startIdx + endIdx) / 2);
+  const bst = new BST(array[midIdx]);
+
+  bst.left = ___constructMinHeightBst(array, startIdx, midIdx - 1);
+  bst.right = ___constructMinHeightBst(array, midIdx + 1, endIdx);
+
+  return bst;
+}
+
+// 오름차순과 이진 트리 특성을 적극활용하자. 중간값을 기준으로 왼쪽과 오른쪽으로 반드시 둘로 나뉜다.
 
 class BST {
   constructor(value) {
