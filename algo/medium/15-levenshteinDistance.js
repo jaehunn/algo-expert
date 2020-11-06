96fcb95157191f24a282831b10108db485278e4e
bangjaehun <qkdwogns98@gmail.com>
2020-11-06 23:12:02 +0900

    levenstein

--- algo/medium/15-levenshteinDistance.js
+++ algo/medium/15-levenshteinDistance.js
@@ -12,7 +12,9 @@ function levenshteinDistance(str1, str2) {
     edits.push(row);
   }
 
-  // 레번스타인 알고리즘
+  // 레번스타인 알고리즘 (다이나믹 프로그래밍)
+  // 1. 끝 글자가 같으면 대각 요소를 그대로 가져간다. (a, ya)
+  // 2. 그렇지 않다면, 대각요소, 상, 하 요소 중 최소값에 연산을 하나 더한다
 
   for (let i = 1; i < str2.length + 1; i += 1) {
     for (let j = 1; j < str1.length + 1; j += 1) {
