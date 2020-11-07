2e80b8866322dc7e4ab9d0e8f97da3fbc2db83f5
bangjaehun <qkdwogns98@gmail.com>
2020-11-07 12:07:59 +0900

    16 kadane

---
+++ algo/medium/16-kadane'sAlgorithm.js
@@ -0,0 +1,11 @@
+// O(n) / O(1)
+function kadanesAlgorithm(array) {
+  let pre = (cur = array[0]);
+
+  for (let i = 1; i < array.length; i += 1) {
+    pre = Math.max(pre + array[i], array[i]); // add or reset
+    cur = Math.max(cur, pre);
+  }
+
+  return cur;
+}
