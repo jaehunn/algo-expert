3e104a79ba25a6e4475663d105be2e0f21488ded
bangjaehun <qkdwogns98@gmail.com>
2020-11-14 21:11:57 +0900

    later 20

--- algo/medium/20-youngestCommonAncestor.js
+++ algo/medium/20-youngestCommonAncestor.js
@@ -7,8 +7,13 @@ class AncestralTree {
   }
 }
 
+// 1. 높이가 다른 자손의 높이를 맞춘다.
+// 2. 부모가 일치할 때까지 거슬러 올라간다. 루트 Ancestor 로 수렴한다.
+
+// lower 한 높이를 가진 자손을 higer 높이의 자손에 맞춘다. (최소 공배수 개념과 비슷)
+
 function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
-  const depthOne = getDescendantDepth(descendantTwo, topAncestor);
+  const depthOne = getDescendantDepth(descendantOne, topAncestor);
   const depthTwo = getDescendantDepth(descendantTwo, topAncestor);
 
   if (depthOne > depthTwo) {
@@ -21,7 +26,35 @@ function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
 
   return backtrackAncestralTree(
     descendantTwo,
-    descendantTwo,
+    descendantOne,
     depthTwo - depthOne
   );
+}
+
+function getDescendantDepth(descendant, topAncestor) {
+  let depth = 0;
+
+  while (descendant !== topAncestor) {
+    depth++;
+
+    descendant = descendant.ancestor;
+  }
+
+  return depth;
+}
+
+function backtrackAncestralTree(lowerDescendant, higherDescendant, diff) {
+  while (diff > 0) {
+    lowerDescendant = lowerDescendant.ancestor;
+
+    diff--;
+  }
+
+  while (lowerDescendant !== higherDescendant) {
+    lowerDescendant = lowerDescendant.ancestor;
+
+    higherDescendant = higherDescendant.ancestor;
+  }
+
+  return lowerDescendant;
 }
