552daa3f04628dd6671504339ecc6202559744fe
bangjaehun <qkdwogns98@gmail.com>
2020-11-19 18:56:45 +0900

    again later heap ds

--- algo/medium/21-minHeapConstruction.js
+++ algo/medium/21-minHeapConstruction.js
@@ -4,12 +4,66 @@ class MinHeap {
     this.heap = this.buildHeap(array);
   }
 
-  // wip
-  buildHeap(array) {}
+  // parent = [0 ... (endIndex - 1) / 2]
+  // O(n) / O(1)
+  buildHeap(array) {
+    const rootIndex = ((array.length - 2) / 2) << 0;
 
-  siftDown() {}
+    for (let parentIndex = rootIndex; parentIndex >= 0; parentIndex -= 1) {
+      this.siftDown(parentIndex, array);
+    }
 
-  siftUp() {}
+    return array;
+  }
+
+  // O(log n) / O(1)
+  siftDown(parentIndex, heap) {
+    let leftChildIndex = parentIndex * 2 + 1;
+    while (leftChildIndex < heap.length) {
+      // child
+      let rightChildIndex =
+        parentIndex * 2 + 2 < heap.length ? parentIndex * 2 + 2 : -1;
+
+      let childIndex;
+      if (
+        rightChildIndex !== -1 &&
+        heap[leftChildIndex] < heap[rightChildIndex]
+      )
+        childIndex = rightChildIndex;
+      else childIndex = leftChildIndex;
+
+      // parent
+      if (heap[childIndex] < heap[parentIndex]) {
+        // swap
+        [heap[childIndex], heap[parentIndex]] = [
+          heap[parentIndex],
+          heap[childIndex],
+        ];
+
+        // move
+        parentIndex = childIndex;
+        leftChildIndex = parentIndex * 2 + 1;
+      } else return;
+    }
+  }
+
+  // O(log n) / O(1)
+  siftUp(childIndex, heap) {
+    // parent
+    let parentIndex = ((childIndex - 1) / 2) << 0;
+
+    while (childIndex && heap[childIndex] < heap[parentIndex]) {
+      // swap
+      [heap[childIndex], heap[parentIndex]] = [
+        heap[parentIndex],
+        heap[childIndex],
+      ];
+
+      // move
+      childIndex = parentIndex;
+      parentIndex = ((childIndex - 1) / 2) << 0;
+    }
+  }
 
   peek() {
     return this.heap[0];
