75afd7e8b7713d022db0e9a5f253a35a5e80615e
bangjaehun <qkdwogns98@gmail.com>
2020-12-05 12:28:22 +0900

    10

--- BinarySearchTree/7-BST_construction.js
+++ BinarySearchTree/7-BST_construction.js
@@ -1,4 +1,19 @@
-// recursive approach
+/**
+ * Write BST class for a Binary Search Tree. The class should support:
+ *  - Inserting values with the insert method.
+ *  - Removing values with the remove method; this method should only remove the first instance of a given value.
+ *  - Searching for values with the contains method.
+ *
+ * Note that you can't remove values from a single-node tree.
+ * In other words, calling the remove method on a single-node tree should simply not do anything.
+ *
+ * Each BST node has an integer value, a left child node, and a right child node.
+ * A node is said to be a valid BST node if and only if it satisfies the BST property:
+ * its value is strictly greater than the values of every node to its left;
+ * its value is less than or equal to the values of every node to its right;
+ * and its children nodes are either valid BST nodes themselves or None / null.
+ */
+
 class BST {
   constructor(value) {
     this.value = value;
@@ -11,9 +26,11 @@ class BST {
   insert(value) {
     if (value < this.value) {
       if (this.left) return this.left.insert(value);
+
       this.left = new BST(value);
     } else {
       if (this.right) return this.right.insert(value);
+
       this.right = new BST(value);
     }
 
@@ -36,8 +53,6 @@ class BST {
     }
   }
 
-  // 자신을 가리킬 포인터가 필요하다.
-
   // Average: O(log n) / O(log n)
   // Worst: O(n) / O(n)
   remove(value, parent = null) {
@@ -47,15 +62,14 @@ class BST {
     } else if (value > this.value) {
       if (this.right) this.right.remove(value, this);
 
-      // find: this.value === value
+      // this.value === value
     } else {
-      // swap(this, getLeftLeaf) -> delete(getLeftLeaf)
       if (this.left && this.right) {
         this.value = this.right.getLeftLeaf();
 
         this.right.remove(this.value, this);
       } else if (!parent) {
-        // left || right: unbalanced, root node
+        // unbalanced tree + root
         if (this.left) {
           this.value = this.left.value;
           this.right = this.left.right;
@@ -65,9 +79,9 @@ class BST {
           this.left = this.right.left;
           this.right = this.right.right;
         }
-        // single node: NOOP
+        // no children(single node): NOOP
       } else {
-        // unbalanced, etc or leaf
+        // unbalanced tree + no root
         if (parent.left === this) {
           parent.left = this.left ? this.left : this.right;
         } else if (parent.right === this) {
@@ -86,8 +100,6 @@ class BST {
   }
 }
 
-// WIP
-// iteration approach
 class BST {
   constructor(value) {
     this.value = value;
@@ -110,12 +122,12 @@ class BST {
 
         currentNode = currentNode.left;
       } else {
-        // if these nodes value be same, then add to right child
         if (!currentNode.right) {
           currentNode.right = new BST(value);
 
           return this;
         }
+
         currentNode = currentNode.right;
       }
     }
@@ -139,23 +151,64 @@ class BST {
     return false;
   }
 
+  // Average: O(log n) / O(1)
+  // Worst: O(n) / O(1)
   remove(value, parentNode = null) {
     let currentNode = this;
 
     while (currentNode) {
       if (value < currentNode.value) {
         parentNode = currentNode;
+
         currentNode = currentNode.left;
       } else if (value > currnetNode.value) {
         parentNode = currentNode;
-        currentNode = currentnode.right;
+
+        currentNode = currentNode.right;
+      } else {
+        // 1. both
+        // 2. one (root, no-root)
+        if (currentNode.left && currentNode.right) {
+          currentNode.value = currentNode.right.getMinValue();
+
+          currentNode.right.remove(currentNode.value, currentNode);
+        } else if (!parentNode) {
+          if (currentNode.left) {
+            currentNode.value = currentNode.left.value;
+
+            // currentNode.left 를 먼저 설정하면 부수효과가 일어난다
+            currentNode.right = currentNode.left.right;
+            currentNode.left = currentNode.left.left;
+          } else if (currentNode.right) {
+            currentNode.value = currentNode.right.value;
+
+            currentNode.left = currentNode.right.left;
+            currentNode.right = currentNode.right.right;
+          }
+
+          // single node: NOOP
+        } else if (parentNode.left === currentNode) {
+          parentNode.left = currentNode.left
+            ? currentNode.left
+            : currentNode.right;
+        } else if (parentNode.right === currentNode) {
+          parentNode.right = currentNode.left
+            ? currentNode.left
+            : currentNode.right;
+        }
       }
     }
+
+    return this;
   }
 
-  getLeftLeaf() {
-    if (!this.left) return this.value;
+  getMinValue() {
+    let currentNode = this;
 
-    return this.left.getLeftLeaf();
+    while (currentNode.left) {
+      currentNode = currentNode.left;
+    }
+
+    return currentNode.value;
   }
 }
