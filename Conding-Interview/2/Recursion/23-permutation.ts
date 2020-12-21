6787f35b11abf8f54c34cde543d36a1b4ad1e191
bangjaehun <qkdwogns98@gmail.com>
2020-12-21 19:05:00 +0900

    recur...

--- Conding-Interview/2/Recursion/23-permutation.ts
+++ Conding-Interview/2/Recursion/23-permutation.ts
@@ -1,45 +1,34 @@
-// 3P3 = 3! = 3 * 2 * 1
-// [1, 2 ,3]
-
-// 1
-// 2 3 -> 1 2 3
-// 3 2 -> 1 3 2
-
-// 2
-// 1 3 -> 2 1 3
-// 3 1 -> 2 3 1
-
-// 3
-// 1 2 -> 3 1 2
-// 2 1 -> 3 2 1
+/**
+ * Write a function that takes in an array of unique integers
+ * and returns an array of all permutations of those integer in no particular order.
+ *
+ * If the input array is empty, the function should return an empty array
+ */
 
 // upper bound: O(n^2 * n!) / O(n * n!)
 // roughly: O(n * n!) / O(n * n!)
-function getPermutations(array) {
-  const permutations = [];
+function getPermutations(array: number[]) {
+  const permutations: number[][] = [];
 
   permutationsHelper(array, [], permutations);
 
   return permutations;
 }
 
-// permutation 과 combination 그리고 각각의 repetition 까지 전부 이해하고 스스로 구현할 줄 알아야한다.
-// 가급적이면 함수를 분리해서 혹시모를 공유 변수 오염에 대비한다.
-
-// 변수를 사용하지않고 파라미터로 바로 넘길때는 객체에 대해서는 immutable 한 연산 (새로운 객체가 생성되는) 을 써야한다. (주의!)
-
-function permutationsHelper(array, currentPermutation, permutations) {
+function permutationsHelper(
+  array: number[],
+  currentPermutation: number[],
+  permutations: number[][]
+) {
   if (!array.length && currentPermutation.length) {
     permutations.push(currentPermutation);
   } else {
     for (let index = 0; index < array.length; index += 1) {
-      // exclude array[index]
       const prefix = array.slice(0, index);
       const suffix = array.slice(index + 1);
 
-      // recur
       permutationsHelper(
-        prefix.concat(suffix),
+        prefix.concat(suffix), // remain
         currentPermutation.concat([array[index]]),
         // push 는 mutate 다.
         permutations
@@ -48,10 +37,9 @@ function permutationsHelper(array, currentPermutation, permutations) {
   }
 }
 
-// another answer LMTP
-
+// wip...
 // O(n * n!) / O(n * n!)
-function _getPermutations(array) {
+function _getPermutations(array: number[]) {
   const permutations = [];
 
   _permutationsHelper(0, array, permutations);
@@ -59,17 +47,21 @@ function _getPermutations(array) {
   return permutations;
 }
 
-function _permutationsHelper(startIndex, array, permutations) {
+function _permutationsHelper(
+  startIndex: number,
+  array: number[],
+  permutations: number[][]
+) {
   if (startIndex === array.length - 1) {
-    permutations.push(array.slice()); // slice() 는 clone 이다.
+    permutations.push(array.slice());
   } else {
     for (let index = startIndex; index < array.length; index += 1) {
       [array[index], array[startIndex]] = [array[startIndex], array[index]];
 
-      _permutationsHelper(index + 1, array, permutations);
+      _permutationsHelper(startIndex + 1, array, permutations);
 
       // revert
-      [(array[index], array[startIndex])] = [array[startIndex], array[index]];
+      [array[index], array[startIndex]] = [array[startIndex], array[index]];
     }
   }
 }
