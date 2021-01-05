23226afcfba21ba2fc0585a48a409b5bf6f93315
bangjaehun <qkdwogns98@gmail.com>
2021-01-05 10:59:34 +0900

    dfs bfs...

--- Conding-Interview/3/Graph/18-breadthFirstSearch.ts
+++ Conding-Interview/3/Graph/18-breadthFirstSearch.ts
@@ -1,22 +1,39 @@
-class Node {
-  constructor(name) {
+/**
+ * You're given a Node class that has a name and an array of optional children nodes.
+ * When put together, nodes form an acyclic tree-like structure
+ *
+ * Implement the breadthFirstSearch method on the Node class, which takes in an empty array,
+ * traverses the tree using the Breath-first Search approach (specifically navigating the tree from left to right),
+ * stores all of the nodes' names in the input array, and returns it.
+ */
+
+export class Node {
+  name: string;
+  children: Node[];
+
+  constructor(name: string) {
     this.name = name;
     this.children = [];
   }
 
-  addChild(name) {
-    this.children.push(new Node(name));
+  addChild(name: string) {
+    const newNode = new Node(name);
+
+    this.children.push(newNode);
+
     return this;
   }
 
-  breadthFirstSearch(array) {
-    const queue = [this];
+  // O(v + e) / O(v)
+  breadthFirstSearch(array: string[]) {
+    const queue: Node[] = [this]; // start root
     while (queue.length) {
       const currentNode = queue.shift(); // dequeue
 
-      array.push(currentNode.name); // search value
+      // store
+      array.push(currentNode.name);
 
-      // children enqueue
+      // enqueue childrens
       for (const children of currentNode.children) {
         queue.push(children);
       }
