3add6d8eaa214853ae76a147832e299eab96fe0e
bangjaehun <qkdwogns98@gmail.com>
2021-01-06 11:02:49 +0900

    graph problem...

--- Conding-Interview/3/Graph/19-riverSizes.ts
+++ Conding-Interview/3/Graph/19-riverSizes.ts
@@ -1,14 +1,22 @@
-// 해당 노드를 거쳤는지 기록해야한다.
-// 기록하게되면, 전체 노드를 순회하는 방식으로 짜더라도 continue guard 로 거를 수 있다.
-
-// 기준노드로부터 상하좌우 노드를 유효한지 평가한다음 새로운 기준점으로 세팅한다. 이를 반복
+/**
+ * You're given a two-dimensional array (a matrix) of potentially unequal height and width containing only 0s and 1s.
+ * Each 0 represents land, and each 1 represents part of a river.
+ *
+ * A river consists of any number of 1s that are either horizontally or vertically adjacent (but not diagonally adjacent)
+ * The number of adjacent 1s forming a river determine its size.
+ *
+ * Note that a river an twist. In other words, it doesn't have to be a straight vertical line or a straight horizontal line;
+ * it can be L-shaped, for example.
+ *
+ * Write a function that returns an array of the sizes fo all rivers represented in the input matrix.
+ * The sizes don't need to be in any particular order.
+ */
 
 // O(wh) / O (wh)
-function riverSizes(matrix) {
-  const sizes = []; // result
-  const visited = matrix.map((row) => row.map(() => false)); // visited matrix
+function riverSizes(matrix: number[][]) {
+  const sizes: number[] = [];
+  const visited: boolean[][] = matrix.map((row) => row.map(() => false));
 
-  // 1. 기준점 설정
   for (let i = 0; i < matrix.length; i += 1) {
     for (let j = 0; j < matrix[0].length; j += 1) {
       if (visited[i][j]) continue; // guard
@@ -20,7 +28,13 @@ function riverSizes(matrix) {
   return sizes;
 }
 
-function traverseNode(i, j, matrix, visited, sizes) {
+function traverseNode(
+  i: number,
+  j: number,
+  matrix: number[][],
+  visited: boolean[][],
+  sizes: number[]
+) {
   let currentSize = 0;
   const nodes = [[i, j]];
 
@@ -30,26 +44,32 @@ function traverseNode(i, j, matrix, visited, sizes) {
     i = currentNode[0];
     j = currentNode[1];
 
-    // guard
+    // cond 1
     if (visited[i][j]) continue;
+
     visited[i][j] = true;
 
+    // cond 2
     if (matrix[i][j] === 0) continue;
+
     currentSize++;
 
-    // 2. 기준점으로부터 상하좌우 노드 확인, 있다면 새로운 기준점으로 가지치기
     const unvisitedNodes = getUnvisitedNodes(i, j, matrix, visited);
     for (const node of unvisitedNodes) {
       nodes.push(node);
     }
   }
 
-  // 최초 기준점에서 뻗어나간 사이즈를 결과 배열에 푸시
-  if (currentSize > 0) sizes.push(currentSize);
+  if (currentSize) sizes.push(currentSize);
 }
 
-function getUnvisitedNodes(i, j, matrix, visited) {
-  const unvisitedNodes = [];
+function getUnvisitedNodes(
+  i: number,
+  j: number,
+  matrix: number[][],
+  visited: boolean[][]
+) {
+  const unvisitedNodes: [number, number][] = []; // [[number, number], ...]
 
   // up down
   if (i > 0 && !visited[i - 1][j]) unvisitedNodes.push([i - 1, j]);
@@ -59,7 +79,7 @@ function getUnvisitedNodes(i, j, matrix, visited) {
   // left right
   if (j > 0 && !visited[i][j - 1]) unvisitedNodes.push([i, j - 1]);
   if (j < matrix[0].length - 1 && !visited[i][j + 1])
-    unvisitedNodes.push([i][j + 1]);
+    unvisitedNodes.push([i, j + 1]);
 
   return unvisitedNodes;
 }
