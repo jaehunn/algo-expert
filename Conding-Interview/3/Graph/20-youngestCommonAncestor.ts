1f33947e4245f298f45da1920daab3f00ebc8787
bangjaehun <qkdwogns98@gmail.com>
2021-01-07 10:51:44 +0900

    backtrack...

--- Conding-Interview/3/Graph/20-youngestCommonAncestor.ts
+++ Conding-Interview/3/Graph/20-youngestCommonAncestor.ts
@@ -1,59 +1,70 @@
-// wip
-// O(depth) / O(1)
+/**
+ * You're given three inputs, all of which are instances of an AncestralTree class that have an ancestor property pointing to their youngest ancestor.
+ * The first inputs is the top ancestor in an ancestral tree (i.e., the only instance that has no ancestor--its ancestor property points to None / null),
+ * and the other two inputs are descendants in the ancestral tree.
+ *
+ * Write a function that returns the youngest common ancestor to the two descendants.
+ *
+ * Note that a descendant is considered its own ancestor.
+ * So in the simple ancestral tree below, the youngest common ancestor to nodes A and B is node A.
+ */
+
+// 1. depth 를 맞춘다
+// 2. 같은 부모가 될때까지 추적한다
+
 class AncestralTree {
-  constructor(name) {
+  name: string;
+  ancestor: AncestralTree | null;
+
+  constructor(name: string) {
     this.name = name;
     this.ancestor = null;
   }
 }
 
-// 1. 높이가 다른 자손의 높이를 맞춘다.
-// 2. 부모가 일치할 때까지 거슬러 올라간다. 루트 Ancestor 로 수렴한다.
-
-// lower 한 높이를 가진 자손을 higer 높이의 자손에 맞춘다. (최소 공배수 개념과 비슷)
-
-function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
+// O(d) / O(1)
+function getYoungestCommonAncestor(
+  topAncestor: AncestralTree,
+  descendantOne: AncestralTree,
+  descendantTwo: AncestralTree
+) {
   const depthOne = getDescendantDepth(descendantOne, topAncestor);
   const depthTwo = getDescendantDepth(descendantTwo, topAncestor);
 
-  if (depthOne > depthTwo) {
-    return backtrackAncestralTree(
-      descendantOne,
-      descendantTwo,
-      depthOne - depthTwo
-    );
-  }
-
-  return backtrackAncestralTree(
-    descendantTwo,
-    descendantOne,
-    depthTwo - depthOne
-  );
+  return depthOne > depthTwo
+    ? backtrackAncestralTree(descendantOne, descendantTwo, depthOne - depthTwo)
+    : backtrackAncestralTree(descendantTwo, descendantOne, depthTwo - depthOne);
 }
 
-function getDescendantDepth(descendant, topAncestor) {
+function getDescendantDepth(
+  descendant: AncestralTree,
+  topAncestor: AncestralTree
+) {
   let depth = 0;
-
   while (descendant !== topAncestor) {
     depth++;
 
-    descendant = descendant.ancestor;
+    descendant = descendant.ancestor!;
   }
 
   return depth;
 }
 
-function backtrackAncestralTree(lowerDescendant, higherDescendant, diff) {
+function backtrackAncestralTree(
+  lowerDescendant: AncestralTree,
+  higherDescendant: AncestralTree,
+  diff: number
+) {
   while (diff > 0) {
-    lowerDescendant = lowerDescendant.ancestor;
+    lowerDescendant = lowerDescendant.ancestor!;
 
     diff--;
   }
 
   while (lowerDescendant !== higherDescendant) {
-    lowerDescendant = lowerDescendant.ancestor;
+    lowerDescendant = lowerDescendant.ancestor!;
 
-    higherDescendant = higherDescendant.ancestor;
+    higherDescendant = higherDescendant.ancestor!;
   }
 
   return lowerDescendant;
